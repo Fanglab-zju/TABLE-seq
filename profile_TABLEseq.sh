97b5abe23eedfbd6a018e00e44fcb4cf75310059
ytang0831 <56997543+ytang0831@users.noreply.github.com>
2022-12-06 02:35:49 +0000

    Update profile_TABLEseq.sh

--- profile_TABLEseq.sh
+++ profile_TABLEseq.sh
@@ -1,15 +1,15 @@
-fasta="/share/home/fanglab/tangyin/data/hg19/hg19.fa"
-gtf="/share/home/fanglab/tangyin/data/hg19/hg19.refGene.gtf"
-hgtxt="/share/home/fanglab/tangyin/data/hg19/hg19.chrom.sizes.txt"
+fasta="/share/home/fanglab/tangyin/data/mm9/mm9.fa"
+gtf="/share/home/fanglab/tangyin/data/mm9/mm9.refGene.gtf"
+mmtxt="/share/home/fanglab/tangyin/data/mm9/mm9.chrom.sizes.txt"
 
 
-#loadGenome.pl -name hg19 -org human -fasta $fasta -gtf $gtf
+#loadGenome.pl -name mm9 -org mouse -fasta $fasta -gtf $gtf
 
 for file in TABLEseq.sorted.bam dUTP.sorted.bam
 do
         name=${file%%.sorted.bam}
         samtools view -h $file > $name.sam
-makeTagDirectory $name/ -genome hg19 -format sam -sspe -checkGC $name.sam
+makeTagDirectory $name/ -genome mm9 -format sam -sspe -checkGC $name.sam
 makeUCSCfile $name/ -o bedGraph/${name}-.bedGraph -strand -
 makeUCSCfile $name/ -o bedGraph/${name}+.bedGraph -strand +
 gunzip bedGraph/${name}-.bedGraph.gz
@@ -18,10 +18,10 @@ sed -i '1d' bedGraph/${name}-.bedGraph
 sed -i '1d' bedGraph/${name}+.bedGraph
 sort -k1,1 -k2,2n bedGraph/${name}-.bedGraph > bedGraph/${name}-.sorted.bedGraph
 # conver bedgraph to bigwig
-bedGraphToBigWig bedGraph/${name}-.sorted.bedGraph $hgtxt bedGraph/${name}-.bw
+bedGraphToBigWig bedGraph/${name}-.sorted.bedGraph $mmtxt bedGraph/${name}-.bw
 #rm bedGraph/${name}-.sorted.bedGraph
 sort -k1,1 -k2,2n bedGraph/${name}+.bedGraph > bedGraph/${name}+.sorted.bedGraph
         # conver bedgraph to bigwig
-bedGraphToBigWig bedGraph/${name}+.sorted.bedGraph $hgtxt bedGraph/${name}+.bw
-annotatePeaks.pl tss hg19 -size 4000 -hist 10 -pc 3 -d ${name}/ > ${name}.output.txt
+bedGraphToBigWig bedGraph/${name}+.sorted.bedGraph $mmtxt bedGraph/${name}+.bw
+annotatePeaks.pl tss mm9 -size 4000 -hist 10 -pc 3 -d ${name}/ > ${name}.output.txt
 done
